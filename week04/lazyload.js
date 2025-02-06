ee55b0e8e987f1ba528beee2f829fdb3db5cede0
rll882571 <del22007@byui.edu>
2025-02-06 11:10:00 -0300

    hi

---
+++ week04/lazyload.js
@@ -0,0 +1,7 @@
+// Atualiza o rodapé com o ano atual
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Atualiza o rodapé com a data da última modificação
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
+
+
