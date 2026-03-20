f10913c4a21e930379516e1d26695599b6209137
rfl882571 <rfl882571@gmail.com>
2026-03-20 16:54:28 -0300

    g

---
+++ styles/week02/scripts/debugging.js
@@ -0,0 +1,31 @@
+
+
+const radiusOutput = document.getElementById('radius');
+const areaOutput = document.querySelector('#area'); 
+
+let area = 0;
+const PI = 3.14159;
+let radius = 10;
+
+area = PI * radius * radius;
+console.log('Área do círculo:', area);
+
+
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
+
+radius = 20; 
+area = PI * radius * radius;
+
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
+
+const circuloOutput = document.getElementById('circulo');
+
+if (radius > 15) {
+    circuloOutput.textContent = 'Grande';
+
+}
+else {
+    circuloOutput.textContent = 'Pequeno';
+}
